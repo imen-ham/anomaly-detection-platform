246bad3ddd42150756bab18dfc4ec9033d43876a
imen-ham <imenhammiche55@gmail.com>
2026-04-18 20:08:51 +0200

    fix: retry connexion DB et seed automatique au démarrage

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,14 +2,27 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
-
+import time
 
 DATABASE_URL = os.environ.get(
     "DATABASE_URL",
     "postgresql+pg8000://admin:admin123@postgres:5432/anomaly_db"
 )
 
-engine = create_engine(DATABASE_URL)
+def create_engine_with_retry():
+    retries = 10
+    for i in range(retries):
+        try:
+            engine = create_engine(DATABASE_URL)
+            engine.connect()
+            print("Connexion DB réussie ✓")
+            return engine
+        except Exception as e:
+            print(f"Tentative {i+1}/{retries} - DB pas prête, attente 3s...")
+            time.sleep(3)
+    raise Exception("Impossible de se connecter à la DB après 10 tentatives")
+
+engine = create_engine_with_retry()
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
